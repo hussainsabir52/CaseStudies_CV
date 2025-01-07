36b958d86955d66142d88e44e7ad9cb831445394
Mustaali Hussain <mustaali20may@gmail.com>
2025-01-07 15:55:07 +0100

    fix 6

--- helper/qwen.py
+++ helper/qwen.py
@@ -2,5 +2,5 @@ from ninept import qwen
 
 def get_cross_validation_technique(prompt):
     print(prompt)
-    qwen_res = qwen(prompt,role='you are a cross validation expert')
-    return qwen_res
+    # qwen_res = qwen(prompt,role='you are a cross validation expert')
+    return prompt
